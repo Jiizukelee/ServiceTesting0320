680e0ee367b0e933f2f0c33387518167454bf84b
Jiizuke Lee <806777178@qq.com>
2019-03-22 09:12:23 +0800

    update

--- src/main/java/com/bigger/zxpjmall/wework/contact/Department.java
+++ src/main/java/com/bigger/zxpjmall/wework/contact/Department.java
@@ -7,7 +7,7 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 
 public class Department {
-    public Response list(String id){
+    public Response list(int id){
          return given().log().all()
                 .param("access_token", Wework.getToken())
                 .param("id", id)
@@ -27,5 +27,53 @@ public class Department {
                 .then().log().all()
                 .statusCode(200).extract().response();
     }
+
+    public Response update(int id, String name){
+        String body = JsonPath.parse(this.getClass().getResourceAsStream("/data/update.json"))
+                .set("$.id", id).set("$.name", name).jsonString();
+        return given().log().all()
+                .queryParam("access_token",Wework.getToken())
+                .body(body)
+                .when().log().all()
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/update")
+                .then().log().all()
+                .extract().response();
+    }
+
+    public Response update(int id, int pid){
+        String body = JsonPath.parse(this.getClass().getResourceAsStream("/data/update.json"))
+                .set("$.id", id).set("$.parentid", pid).jsonString();
+        return given().log().all()
+                .queryParam("access_token",Wework.getToken())
+                .body(body)
+                .when().log().all()
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/update")
+                .then().log().all()
+                .extract().response();
+    }
+
+    public Response update(int id, String name, int pid){
+        String body = JsonPath.parse(this.getClass().getResourceAsStream("/data/update.json"))
+                .set("$.id", id).set("$.parentid", pid).set("$.name",name).jsonString();
+        return given().log().all()
+                .queryParam("access_token",Wework.getToken())
+                .body(body)
+                .when().log().all()
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/update")
+                .then().log().all()
+                .extract().response();
+    }
+
+    public Response delete(int id){
+        return given().log().all()
+                .param("access_token",Wework.getToken())
+                .param("id",id)
+                .when()
+                .get("https://qyapi.weixin.qq.com/cgi-bin/department/delete")
+                .then().log().all()
+                .extract().response();
+    }
+
+
 
 }
--- src/test/java/com/bigger/zxpjmall/wework/contact/DepartmentTest.java
+++ src/test/java/com/bigger/zxpjmall/wework/contact/DepartmentTest.java
@@ -18,10 +18,10 @@ class DepartmentTest {
 
     @Test
     void list() {
-        department.list("2")
+        department.list(2)
                 .then().statusCode(200)
                 .body("errcode",equalTo(0),"department.name[1]",equalTo("空气猫"));
-        department.list("6")
+        department.list(6)
                 .then().statusCode(200)
                 .body("errcode",equalTo(0),"department[0].id",equalTo(6));
     }
@@ -30,5 +30,28 @@ class DepartmentTest {
     void create() {
         department.create("Jiizuketest_03212", 5).then().body("errcode", equalTo(0));
         department.create("Jiizuketest_03213", 5).then().body("errcode", equalTo(0));
+    }
+
+    @Test
+    void update() {
+        department.update(8, "songjiangyanfa05").then().body("errcode", equalTo(0));
+        department.update(8, 6).then().body("errcode", equalTo(0));
+        department.update(100, "keji01", 3).then().body("errcode", equalTo(0));
+    }
+
+    @Test
+    void delete() {
+        department.delete(8).then().body("errcode",equalTo(0));
+    }
+
+    @Test
+    void createUpdateDelete(){
+        department.create("Testapi0001",12).then().body("errcode", equalTo(0));
+        Integer id = department.list(12)
+                .path("department.find{department -> department.name == \"Testapi0001\"}.id");
+        department.update(id,"Testapi1001",12).then().body("errcode", equalTo(0));
+//        department.delete(id).then().body("errcode", equalTo(0));
+//        System.out.println(id.getClass());
+//        System.out.println(id);
     }
 }
